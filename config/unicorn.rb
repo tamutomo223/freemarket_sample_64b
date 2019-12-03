aeabe3dd5c2cf1af85b008acd9066fdd87e604ff
tamutomo223 <57092560+tamutomo223@users.noreply.github.com>
2019-12-03 23:44:16 +0900

    デプロイ用

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,7 +2,7 @@ app_path = File.expand_path('../../../', __FILE__)
 
 worker_processes 1
 # currentを指定
-working_directory "#{app_path}/current"
+working_directory app_path
 
 # それぞれ、sharedの中を参照するよう変更
 listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
