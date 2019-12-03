bc75facad9f4f0daff7d23c5b93508004acf1fa6
tamutomo223 <57092560+tamutomo223@users.noreply.github.com>
2019-12-03 20:37:55 +0900

    デプロイ用

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
 
-  server '3.115.222.173', user: 'ec2-user', roles: %w{app db web}
+  
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
