73ff11f9a880c05781cefdfb3315e1c5a1d625cd
tamutomo223 <57092560+tamutomo223@users.noreply.github.com>
2019-12-04 14:46:33 +0900

    sell画面

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,8 @@ class ItemsController < ApplicationController
   def index
 
   end  
+
+  def sell
+
+  end  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
+  get "/sell" , to: "items#sell", as: "sell_items"
 end
