a545a4304923be8ad43d3f7649d071ffdef7fb5a
tamutomo223 <57092560+tamutomo223@users.noreply.github.com>
2019-12-05 17:01:24 +0900

    トップビュー完成
    リファクタリング
    カラーコードなども意識

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,18 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
+//= require jquery
+//= require jquery_ujs
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+$(function() {
+  $('.slide').slick({
+      dots: true,
+      autoplay: true,
+      autoplaySpeed: 6000,
+  });
+});
