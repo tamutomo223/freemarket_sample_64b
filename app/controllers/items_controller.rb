11751d018582931a0bda48e9cef18550e788b23a
siina-create <ryotagfr5@gmail.com>
2019-12-05 21:50:45 +0900

    出品画面作成途中２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,22 +5,28 @@ class ItemsController < ApplicationController
 
   def sell
     @item = Item.new
-    @image = Image.new
+    @item.images.build
   end  
 
   def exhibit
-    @item = Item.create(item_params)
     binding.pry
-    @image = Image.create(item_id: @item.id, image_url:image_params[:image_url])
+    @item = Item.new(item_params)
+    @item.save
+    #@image = Image.create(item_id: @item.id, image_url:image_params[:image_url])
   end
 
   private
   def item_params
     #次の4つは本来は出品ページから入れる物ではないため、昨日実装後に削除
     #:order_id,:user_id,:size_type,:view
-    params.permit(:item_name,:explain,:status,:fee,:s_prefecture,:s_date,:price,:size,:category_id,:brand_id,:order_id,:user_id,:size_type,:view)
+    params.require(:item).permit(:item_name,:explain,:status,:fee,:s_prefecture,:s_date,:price,:size,:category_id,:brand_id,:order_id,:user_id,:size_type,:view,
+      images_attributes:[:imege_url])
+
+    # params.require(:item).permit(:item_name, images_attributes:[:item][:images_attributes]["0"][:imege_url])
+    biding.pry
   end
-  def image_params
-    params.require(:images).permit(:image_url)
+
+  def image_params(params)
+    params.permit(:image_url)
   end
 end
