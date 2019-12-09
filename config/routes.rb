534fa1b2a54238da8848b1b9074c3cd535da0215
taiki1446 <kbkr0921@gmail.com>
2019-12-09 17:14:35 +0900

    不要なルーティングの削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  # devise_scope :user do
-  #   get "signup/registration", to: "devise/registrations#new"
-  # end
   root to: "items#index"
 
   #itemsコントローラ内に作ったオリジナルの変数にルーティングの設定をしたい場合,次の①,②,③に記述してください
@@ -16,8 +13,6 @@ Rails.application.routes.draw do
       #②URIPatternにidが欲しい場合はcollectionではなくmember内に記述
     end
   end
-
-  get "/signup" , to: "users#signup", as: "signup"
 
   resources :users ,only:[:mypage, :signup] do
     collection do
