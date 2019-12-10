da19e4af46d3c407a79c387a83fae3dbb6706d46
siina-create <ryotagfr5@gmail.com>
2019-12-10 21:20:53 +0900

    商品出品機能ー画像複数投稿実装
    #What
    画像を複数枚フォームにいれてデータベースに登録できるようにした
    画像入力フォームの画像表示未実装
    フォームのバリデーション未実装
    
    #Why
    出品機能作成のため

--- app/assets/javascripts/sell.js
+++ app/assets/javascripts/sell.js
@@ -1,10 +1,19 @@
 $(function() {
+  //画像複数保存用の配列
+  //if(document.URL.match("/sell")){
+    //window.onload = function(){
+    //alert('配列作成');
+    //console.log(image_array)
+    //}
   
+  let image_array = new Array();//配列の宣言送信時に初期化する
+
+  //フォームに入れた画像を表示
   $('.input-image-box').change(function(){
     //フォームに入れた画像の確認用
-    console.log(image)
+    var a = $(".input-image-box").val()
+    console.log(a)
     console.log(image.files[0]);
-    
     //フォームに入れた画像を表示するためのurlを作る
     var image_path = URL.createObjectURL(image.files[0]);
     var html = `<div class="image-preview-box" >
@@ -13,14 +22,31 @@ $(function() {
     $('.preview').append(html);
     URL.revokeObjectURL(".image_preview.src");
 
-  });
-
-  $('.form-btn').onclick(function(){
-    console.log("a")
-
+    //画像の複数保存
+    image_array.push(image_path);
+    console.log(image_array)
 
   });
 
-    
- 
+  
+
+
+  //手数料の計算
+  $('.price').change(function(){
+    //フォームに入れた数字
+    var price = $(this).val()
+    console.log(`フォームに入れた数字は${price}`)
+
+    //手数料
+    var charge = parseInt(price)*0.10;
+    console.log(`手数料は${charge}`)
+
+    //販売利益
+    var profit = parseInt(price)-parseInt(charge)
+    console.log(`販売利益は${profit}`)
+    $('.charge__result').append(charge);
+    $('.profit__result').append(profit);
+  });
+  
+  
 });
