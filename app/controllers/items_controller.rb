b493d3226aff627dee94fc9e5561264b1d4fa086
taiki1446 <kbkr0921@gmail.com>
2019-12-10 16:40:25 +0900

    10レコードの取得方法の変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   def index
-    @items = Item.all.includes(:images).order("created_at DESC")
+    @items = Item.all.includes(:images).order("created_at DESC").limit(10)
   end  
 
   def sell
