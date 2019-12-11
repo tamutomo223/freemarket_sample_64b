6750ceb1f710485408f91c87df77a89a6e038ec4
mile10032 <57488122+mile10032@users.noreply.github.com>
2019-12-11 21:39:21 +0900

    削除しました。

--- vendor/bundle/ruby/2.5.0/gems/active_hash-3.0.0/lib/active_hash/version.rb
+++
@@ -1,5 +0,0 @@
-module ActiveHash
-  module Gem
-    VERSION = "3.0.0"
-  end
-end
--- vendor/bundle/ruby/2.5.0/gems/ffi-1.11.3/ext/ffi_c/libffi-x86_64-darwin19/include/ffitarget.h
+++
@@ -1,1 +0,0 @@
-/Users/shun/Desktop/a/freemarket_sample_64b/vendor/bundle/ruby/2.5.0/gems/ffi-1.11.3/ext/ffi_c/libffi/src/x86/ffitarget.h
