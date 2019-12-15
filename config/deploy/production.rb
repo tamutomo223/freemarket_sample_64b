7239d30bf6d9fb6696244703537ef0f9be4f4c74
tamutomo223 <57092560+tamutomo223@users.noreply.github.com>
2019-12-15 22:28:42 +0900

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,1 +1,10 @@
-server '3.115.222.173', user: 'ec2-user', roles: %w{app db web}
+server '3.115.222.173', 
+user: 'ec2-user', 
+roles: %w{app db web}
+
+ssh_otions: {
+  port: 22022,
+  keys: %w(~/.ssh/tamutomo223.pem),
+  user: "odatakashi",
+  forward_agent: true,
+}
