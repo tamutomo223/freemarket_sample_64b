8849b881d018b0045cb127478e6ad16759907b64
tamutomo223 <57092560+tamutomo223@users.noreply.github.com>
2019-12-16 13:01:07 +0900

    デプロイ

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,12 +28,12 @@ set :keep_releases, 5
 set :linked_files, fetch(:linked_files, []).push("config/master.key")
 set :linked_files, %w{ config/credentials.yml.enc }
 
-set :default_env, {
-  rbenv_root: "/usr/local/rbenv",
-  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
-  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
-  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
-}
+# set :default_env, {
+#   rbenv_root: "/usr/local/rbenv",
+#   path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+#   AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
+#   AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
+# }
 
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
