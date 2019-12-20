1bcbb84edab2d6b9de395fa9284ec0fce0393b1c
taiki1446 <kbkr0921@gmail.com>
2019-12-20 14:35:39 +0900

    カテゴリーメニューの表示

--- app/assets/javascripts/top.js
+++ app/assets/javascripts/top.js
@@ -6,9 +6,9 @@ $(function() {
 
   }); 
 
-  // $(".top__header__content__2__left__category").hover(function(){
-  //   $(".hidden__category").removeClass("none")
-  // },function(){
-  //   $(".hidden__category").addClass("none")
-  // });
+  $(".top__header__content__2__left__category").hover(function(){
+    $(".hidden__category").removeClass("none")
+  },function(){
+    $(".hidden__category").addClass("none")
+  });
 });
